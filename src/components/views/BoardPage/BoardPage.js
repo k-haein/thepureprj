e74b5be1ce239a2aa4ef91e23e0bcede83d5e8c2
k-haein <k.haein96@gmail.com>
2022-12-20 00:33:40 +0900

    첫 커밋 :: 텍스트 띄움

--- src/components/views/BoardPage/BoardPage.js
+++ src/components/views/BoardPage/BoardPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 function BoardPage() {
     return (
     <div>
-        <h1>일단 띄웠으니까 감격부터 하고 시작합시다! 첫화면 띄우기 및 git push</h1>
+        <h1>일단 띄웠으니까 감격부터 하고 시작합시다!  첫화면 띄우기 및 git push</h1>
     </div>
     )
 }
